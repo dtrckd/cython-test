d2cc9c78d63d7ff62b97483772a491babba5e913
dtrckd <ddtracked@gmail.com>
2018-05-23 16:22:48 +0200

    numpy operation

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import numpy as np
 
 if __name__ == '__main__':
 
-    a = np.ones(((10,3)), dtype=int)
+    #a = np.ones(((10,3)), dtype=int)
+    a  = np.array(([1,3,3],[1,4,3],[1,3,4]))
 
     print(a)
     b = test1(a, len(a))
--- test.pyx
+++ test.pyx
@@ -12,6 +12,9 @@ ctypedef np.int_t INT_t
 #def test1(INT_t[:,:] a,  int b): # it returns a memoryview here
 cpdef np.ndarray test1(np.ndarray[INT_t, ndim=2] a,  int b):
 
+    #b = np.linalg.inv(a)
+    #print(b)
+
     cdef int pos = 0
     #print('coucou')
     for i in range(b):
@@ -21,6 +24,7 @@ cpdef np.ndarray test1(np.ndarray[INT_t, ndim=2] a,  int b):
         a[i,0] += a[pos-1, 0]
 
     a = a.T
+    a = a.dot(a.T)
     return a
 
 
